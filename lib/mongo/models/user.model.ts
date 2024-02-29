414dabf110f702be89579136240a249f0b1e82c3
Ava Matthew <washington_ava@yahoo.com>
2024-02-29 17:38:41 -0600

    Updated user schema

--- lib/mongo/models/user.model.ts
+++ lib/mongo/models/user.model.ts
@@ -4,8 +4,8 @@ const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  firstName: { type: String },
+  lastName: { type: String },
 })
 
 const User = models.User || model('User', UserSchema);
