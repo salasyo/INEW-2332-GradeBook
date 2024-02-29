2c7d6d7f94f30f01cd7a6de00d349fe24755d5d2
Ava Matthew <washington_ava@yahoo.com>
2024-02-29 06:12:54 -0600

    Added home links to header and footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,9 +1,14 @@
+import Image from "next/image"
 import Link from "next/link"
 
 const Footer = () => {
   return (
     <footer className="border-t">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-4 text-center sm:flex-row">
+        <Link href='/'>
+          <Image src="/assets/images/exam-paper.svg" alt="logo" width={40} height={40}/>
+        </Link>
+        
         <p>2024 Gradebook. All Rights reserved.</p>
       </div>
     </footer>
--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Header = () => {
   return (
     <header className="w-full border-b">
-      Database Management System
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image src="/assets/images/gradebook.svg" width={128} height={38} alt="Gradebook Logo" />
+        </Link>
+      </div>
+      
     </header>
   )
 }
