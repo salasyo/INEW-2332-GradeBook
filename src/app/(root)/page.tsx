245730d6a9825f052da08c820ea7e57b0eb78146
Ava Matthew <washington_ava@yahoo.com>
2024-03-01 11:28:57 -0600

    Updated homepage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -10,10 +10,22 @@ export default async function Home() {
         <div className="wrapper gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col gap-8">
             <h1 className="h1-bold">Welcome to the Database Management System of Some College!</h1>
-            <p className="flex-center p-regular-20 md:p-regular-24 ">
-              Please choose from the following directory:
-            </p>
-
+            <div>
+              <p className="flex-center p-regular-20 md:p-regular-24">
+                Please choose from the following directory:
+              </p>
+              <SignedOut>
+                <p className="flex-center p-regular-18 md:p-regular-18">
+                  (Login for more options)
+                </p>
+              </SignedOut>
+              <SignedIn>
+                <p className="flex-center p-regular-18 md:p-regular-18 text-primary-500">
+                  Some options require special site privileges. For access issues, contact system administration.
+                </p>
+              </SignedIn>
+            </div>
+            
             <div className="flex flex-col flex-center gap-8">
               <SignedOut>
                 <Button size="lg" asChild className="button w-full sm:w-fit">
@@ -22,10 +34,11 @@ export default async function Home() {
                   </Link>
                 </Button>
               </SignedOut>
+
               <SignedIn>
                 <Button size="lg" asChild className="button w-full sm:w-fit">
-                  <Link href="/admin/dashboard">
-                    Admin Menu
+                  <Link href="/">
+                    Visitor Menu
                   </Link>
                 </Button>
 
@@ -40,6 +53,13 @@ export default async function Home() {
                     Instructor Menu
                   </Link>
                 </Button>
+
+                <Button size="lg" asChild className="button w-full sm:w-fit">
+                  <Link href="/admin/dashboard">
+                    Admin Menu
+                  </Link>
+                </Button>
+
               </SignedIn>
             </div>
             
